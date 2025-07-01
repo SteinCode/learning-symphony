65286782f0a474ca8585e60ced258286c2241e95
SteinCode <rokas.student@gmail.com>
2025-07-01 11:28:08 +0300

    add HomeController.php and install twig

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController
+{
+    #[Route('/')]
+    public function index(): Response{
+         return new Response("Hello world");
+    }
+}
