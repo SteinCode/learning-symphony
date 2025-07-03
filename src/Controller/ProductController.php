22e257dc81c81a45bc82fce6c115417be9eacead
SteinCode <rokas.student@gmail.com>
2025-07-03 11:17:51 +0300

    submit product, install validator

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,14 +6,52 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
+use App\Form\ProductForm;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ProductController extends AbstractController
 {
     #[Route('/product', name: 'product_index')]
     public function index(ProductRepository $repository): Response
-    { 
+    {
         return $this->render('product/index.html.twig', [
             'products' => $repository->findAll(),
         ]);
+    }
+
+    #[Route('/product/{id}', name: 'product_show', requirements: ['id' => '\d+'])]
+    public function show(Product $product): Response
+    {
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/product/new', 'product_new')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductForm::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $manager->persist($product);
+            
+            $manager->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'id' => $product->getId()
+            ]);
+        }
+        return $this->render(
+            '/product/new.html.twig',
+            ['form' => $form],
+        );
     }
 }
